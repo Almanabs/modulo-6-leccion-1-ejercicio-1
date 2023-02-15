37977d0355251832fd14ff149c60a14ad7680057
Almanabs <121205592+Almanabs@users.noreply.github.com>
2023-02-15 03:54:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const moment = require('moment');
+const randomWords = require('random-words');
+moment.locale("ES"); 
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send(`
+  <ul>
+    <li>Día: ${moment().format('dddd')}</li>
+    <li>Número día: ${moment().format('D')}</li>
+    <li>Mes: ${moment().format('MMMM')}</li>
+    <li>Año: ${moment().format('YYYY')}</li> </li>
+    <li>Hora: ${moment().format('H')}</li>
+    <li>Minutos: ${moment().format('mm')}</li>
+    <li>Segundos: ${moment().format('ss')} </li>
+  </ul>
+`)
+})
+
+app.get('/azar', function(req, res){
+  console.log(randomWords())
+  res.send(`<h1>${randomWords({exactly: 1, maxLength: 10})}</h1>`)
+})
+
+app.put('/azar', function(req, res){
+  let randomNumber = Math.floor(Math.random() * (50000 - 10) + 10);
+  res.send(`<h1>${randomNumber}</h1>`)
+})
+
+app.all('*', function(req, res){
+  res.send(`<h1>Aún no estoy preparado para responder al método ${req.method}</h1>`)
+})
+
+app.listen(3000, () => console.log("Servidor escuchando en http://localhost:3000"))
+
+
+console.log(htmlResponse);
